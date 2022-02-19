9242225d83e8611a3aa05d164d1888c7cf52083a
Nave-wata <90523045+Nave-wata@users.noreply.github.com>
2022-02-19 21:43:30 +0900

    時間表示

--- fastapi/controllers.py
+++ fastapi/controllers.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from starlette.requests import Request
 
+import datetime
+
 app = FastAPI()
 
 def base(request: Request):
     return {"message" : "Hello World!"}
 
 def one(request: Request):
-    a: int = 1
-    return {"Number" : {a}}
+    time = datetime.datetime.now()
+    return {"Now" : time}
--- fastapi/urls.py
+++ fastapi/urls.py
@@ -1,4 +1,4 @@
 from controllers import *
 
 app.add_api_route('/', base)
-app.add_api_route('/1/', one)
+app.add_api_route('/now/', one)
